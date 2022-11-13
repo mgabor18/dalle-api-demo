2ea4c698f1028db29cdf2566b2e4d0fe398daae6
mgabor18 <56040199+mgabor18@users.noreply.github.com>
2022-11-13 17:58:33 +0100

    Update generate.js

--- src/generate.js
+++ src/generate.js
@@ -4,29 +4,41 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+// Create configuration for the OpenAI API
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openAI = new OpenAIApi(configuration);
 
-const prompt = 'A pirate cat with a parrot on his shoulders, riding a bike';
+// The image will be created based on this text
+const prompt = 'A realistic black cat in front of a white door';
 
-const result = await openAI.createImage({
+// The API returns with the result, the image can be reached by an URL inside this result
+const creationResult = await openAI.createImage({
   prompt,
-  n: 1,
-  size: '256x256',
+  n: 1, // number of pictures to be created
+  size: '256x256', // size of the picture(s)
 });
 
+// Create a buffer from the image that can be found on the URL so it can be saved
+const imgBuffer = await createImageBuffer(creationResult);
+
+// Save image
+writeFileSync(`./results/generated-${Date.now()}.png`, imgBuffer);
+
+/**
+ * Creates buffer from the image that has been created by the openai dall-e api
+ * @param result creation result of openai picture generation
+ * @returns Buffer
+ */
 async function createImageBuffer(result) {
   const url = result.data.data[0].url;
-
+  //fetch the iamge by the URL
   const imgResult = await fetch(url);
+  //create blob
   const imgBlob = await imgResult.blob();
+  //make a buffer from it
   const imgBuffer = Buffer.from(await imgBlob.arrayBuffer());
   return imgBuffer;
 }
-
-const imgBuffer = await createImageBuffer(result);
-
-writeFileSync(`./results/${Date.now()}.png`, imgBuffer);
